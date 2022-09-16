48d64eebe01b9bcb0ff5618e27908ff47f01ae63
deniso4ka <den.zagrosh@gmail.com>
2022-09-17 01:13:58 +0300

    feat: final without adaptive

--- src/index.ts
+++ src/index.ts
@@ -85,6 +85,7 @@ const clearInfoInput = () => {
    }
 }
 
+
 //Получаем информацию о пользователе .
 
 const getProfileInfo = (): IData | void => {
@@ -93,19 +94,11 @@ const getProfileInfo = (): IData | void => {
    const desc: HTMLInputElement | null = document.querySelector('.inputDesc')
    const image: HTMLInputElement | null = document.querySelector('.inputImgUrl')
    if (name && desc && image) {
-      const img = new Image
-      img.src = image.value
-      img.onload = () => {
-         return {
-            name: name.value || 'Denis',
-            desc: desc.value || 'Genius',
-            image: image.value || 'https://m-chu.ru/wp-content/uploads/2019/04/s1200-7.jpg',
+      return {
+         name: name.value || 'Denis',
+         desc: desc.value || 'Genius',
+         image: image.value || 'https://mineralmarket.ru/img/articlesImg/author.jpg',
 
-         }
-      }
-      img.onerror = () => {
-         alert('Изображение не найдено')
-         clearInfoInput()
       }
    }
 }
@@ -159,31 +152,25 @@ const postElement = (city: string, image: string, id: number, liked: boolean) =>
 const setPost = () => {
    const name: HTMLInputElement | null = document.querySelector('.inputCity')
    const image: HTMLInputElement | null = document.querySelector('.inputImage')
-   if (name && image && name.value && image.value) {
+   if (name && image) {
       const postsStorage = localStorage.getItem('posts')
-      const img = new Image
-      img.src = image.value
-      img.onload = () => {
-         if (postsStorage) {
-            const posts = JSON.parse(postsStorage)
-            const id = new Date().getTime()
-            posts.push({ id, name: name.value, image: image.value, liked: false })
-            localStorage.setItem('posts', JSON.stringify(posts))
-            addPost()
-         } else {
-            const posts = []
-            const id = new Date().getTime()
-            posts.push({ id, name: name.value, image: image.value, liked: false })
-            localStorage.setItem('posts', JSON.stringify(posts))
-            addPost()
-         }
-      }
-      img.onerror = () => {
-         alert('Изображение не найдено')
-         clearInfoInput()
+      if (postsStorage) {
+         const posts = JSON.parse(postsStorage)
+         const id = new Date().getTime()
+         posts.push({ id, name: name.value, image: image.value, liked: false })
+         localStorage.setItem('posts', JSON.stringify(posts))
+         addPost()
+      } else {
+         const posts = []
+         const id = new Date().getTime()
+         posts.push({ id, name: name.value, image: image.value, liked: false })
+         localStorage.setItem('posts', JSON.stringify(posts))
+         addPost()
       }
    }
+
 }
+
 
 
 // Рендер поста
@@ -206,14 +193,12 @@ const addPost = () => {
             if (likeButton) {
                likeButton.addEventListener('click', () => likePost(el.id))
                if (el.liked) {
-                  console.log(1)
                   likeButton.innerHTML = `
 <svg width="22" height="19" viewBox="0 0 22 19" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path fill-rule="evenodd" clip-rule="evenodd" d="M20.2991 1.68186C22.567 3.90213 22.567 7.54338 20.2991 9.78586L10.9804 19L1.6841 9.80806C0.606277 8.72013 0 7.27695 0 5.74496C0 4.21297 0.583823 2.76979 1.6841 1.68186C3.92957 -0.560619 7.61215 -0.560619 9.88007 1.70406L10.9804 2.792L12.0806 1.68186C14.3486 -0.560619 18.0311 -0.560619 20.2991 1.68186Z" fill="black"/>
 </svg>
 `
                } else {
-                  console.log(2)
                   likeButton.innerHTML = `
                   <svg width="22" height="19" viewBox="0 0 22 19" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M20.2991 9.78586C22.567 7.54338 22.567 3.90213 20.2991 1.68186C18.0311 -0.560619 14.3486 -0.560619 12.0806 1.68186L10.9804 2.792L9.88007 1.70406C7.61215 -0.560619 3.92957 -0.560619 1.6841 1.68186C0.583822 2.76979 0 4.21297 0 5.74496C0 7.27695 0.606277 8.72013 1.6841 9.80806L10.9804 19L20.2991 9.78586ZM1.4371 5.74496C1.4371 4.59042 1.8862 3.52469 2.71702 2.72539C3.5703 1.88168 4.67058 1.45983 5.77086 1.45983C6.87114 1.45983 7.97142 1.88168 8.8247 2.72539L10.9804 4.83465L13.136 2.70318C14.8201 1.03798 17.582 1.03798 19.2437 2.70318C20.0521 3.50248 20.5236 4.56821 20.5236 5.72276C20.5236 6.8773 20.0745 7.94303 19.2437 8.74233L10.9804 16.9351L2.71702 8.76453C1.90865 7.94303 1.4371 6.8773 1.4371 5.74496Z" fill="black"/>
